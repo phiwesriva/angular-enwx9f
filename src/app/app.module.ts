a0a65d2b22449024026218c35baf44c70d1f0814
phiwesriva <phiwes@icloud.com>
2020-08-25 03:22:47 +0200

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { TopBarComponent } from './top-bar/top-bar.component';
 
 /*<--const appRoutes: Routes = [
@@ -25,47 +27,12 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 @NgModule({
   imports: [ BrowserModule,
    FormsModule,
-   RouterModule.forRoot([
-      { path: '', component: ProductListComponent },
-      { path: 'products/:productId', component: ProductDetailsComponent },
-      { path: 'cart', component: CartComponent },
-      { path: 'free', component: FreeComponent },
-      { path: 'subsc', component: SubscComponent },
-      { path: 'shipping', component: ShippingComponent },
-      { path: 'news', component: NewsComponent },
-      { path: 'contact', component: ContactComponent },
-      { path: 'podcast', component: PodcastComponent },
-      { path: 'advert', component: AdvertComponent },
-      { path: 'about', component: AboutComponent },
-      { path: 'thanks', component: ThanksComponent },
-      { path: 'comm', component: CommComponent },
-      { path: 'resi', component: ResiComponent },
-      { path: 'inter', component: InterComponent },
-      { path: 'landl', component: LandlComponent },
-      { path: 'readtm', component: ReadtmComponent },
-      { path: 'readchar', component: ReadcharComponent },
-      { path: 'benefits', component: BenefitsComponent },
-      { path: 'rates', component: RatesComponent },
-      { path: 'sections', component: SectionsComponent },
-      { path: 'deeds', component: DeedsComponent },
-      { path: 'remax', component: RemaxComponent },
-      { path: 'hinsure', component: HinsureComponent },
-      { path: 'swaprop', component: SwapropComponent },
-      { path: 'housef', component: HousefComponent },
-      { path: 'commd', component: CommdComponent },
-      { path: 'resid', component: ResidComponent },
-      { path: 'covid', component: CovidComponent },
-      { path: 'loan', component: LoanComponent },
-      { path: 'rent', component: RentComponent },
-      { path: 'rent2', component: Rent2Component },
-      { path: 'repo', component: RepoComponent },
-      { path: 'repof', component: RepofComponent },
-      { path: 'presss', component: PresssComponent },
-      { path: '**', component: PageNotFoundComponent },
-      { path: '',   redirectTo: '/sections', pathMatch: 'full' },
+   /*RouterModule.forRoot([
+     
+        
       
       
-    ])
+    ])*/
   ],
 
   declarations: [ 
